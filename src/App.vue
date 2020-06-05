b8c7d3f1bf49bbbfb42650634f6315a4ef4c0e2d
Stéphen RAVOAVAHY <stephen.niriantso@gmail.com>
2020-06-05 13:31:39 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="antialiased text-gray-900">
+    <div class="bg-gray-200 min-h-screen">
+      <Navbar/>
+      <Card/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Card from './components/Card'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Card
+  },
+}
+</script>
+
+<style src="./assets/tailwind.css"></style>
